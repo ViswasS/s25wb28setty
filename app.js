bd22cd552bb5384ef04adc05c1f762969fc5eda6
Viswas <s.viswas111@gmail.com>
2025-04-02 20:25:29 -0500

    modified index.pug head file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var HologramsRouter = require('./routes/Holograms');
+var gridRouter = require('./routes/grid');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Holograms', HologramsRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
