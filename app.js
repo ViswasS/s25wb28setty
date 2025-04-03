7cf11654c97b5410ef2bdd8a5d7955430640e6d0
Viswas <s.viswas111@gmail.com>
2025-04-02 19:53:51 -0500

    modified index.pug head file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var HologramsRouter = require('./routes/Holograms');
-var Holograms = require('./routes/users');
+
 
 var app = express();
 
